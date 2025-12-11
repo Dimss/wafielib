4270898d7a7c5724a5b911fa6e0cd84b39e8f411
Dmitry Kartsev <dimssss@gmail.com>
2025-12-11 17:20:57 +0200

    add multi threading example

---
+++ src/test-single-thread.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <wafielib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+char *read_file(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Failed to open file");
+        return NULL;
+    }
+
+    // Get file size
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    // Allocate buffer
+    char *content = malloc(size + 1);
+    if (!content) {
+        fclose(file);
+        return NULL;
+    }
+
+    // Read file
+    fread(content, 1, size, file);
+    content[size] = '\0';
+
+    fclose(file);
+    return content;
+}
+
+void *evalu_request(void *ptr) {
+    WafieEvaluationRequest *request = ptr;
+    for (int i = 0; i < 3; i++) {
+        wafie_init_transaction(request);
+        if (request->transaction != NULL) {
+            wafie_process_request_headers(request);
+            wafie_process_request_body(request);
+            wafie_cleanup(request);
+            break;
+        }
+        sleep(1);
+    }
+
+    return NULL;
+}
+
+void *reload_ruleset(void *ptr) {
+    WafieRuleSetConfig cfg[3];
+    cfg[0].protection_id = 1;
+    cfg[0].config_path = "/config1";
+    cfg[1].protection_id = 2;
+    cfg[1].config_path = "/config2";
+    cfg[2].protection_id = 3;
+    cfg[2].config_path = "/config3";
+    wafie_load_rule_sets(cfg, 3);
+    return NULL;
+}
+
+int main() {
+    // WafieRuleSetConfig *cfg = malloc(sizeof(WafieRuleSetConfig) * 2);
+    WafieRuleSetConfig cfg[2];
+    cfg[0].protection_id = 1;
+    cfg[0].config_path = "/config1";
+    // cfg[1].protection_id = 2;
+    // cfg[1].config_path = "/config2";
+
+
+    WafieEvaluationRequestHeader *headers1 = malloc(sizeof(WafieEvaluationRequestHeader) * 2);
+    headers1[0].key = (const unsigned char *) "Host";
+    headers1[0].value = (const unsigned char *) "example.com";
+    headers1[1].key = (const unsigned char *) "User-Agent";
+    headers1[1].value = (const unsigned char *) "KubeGuard/1.0";
+    //
+    //
+    WafieEvaluationRequest request1 = {
+        .client_ip = "192.168.1.2",
+        // .uri = "/etc/passwd",
+        // .uri = "?id=1/**/UNION/**/SELECT/**/'<script>alert(1)</script>'",
+        .uri = "/?id=1/**/UNION/**/SELECT/**/%27%3Cscript%3Ealert(1)%3C/script%3E%27",
+        .http_method = "GET",
+        .http_version = "1.1",
+        .headers_count = 2,
+        .headers = headers1,
+        // .body = "",
+        .protection_id = 1,
+    };
+
+
+    wafie_init();
+    wafie_load_rule_sets(cfg, 1);
+    wafie_init_transaction(&request1);
+    wafie_process_request_headers(&request1);
+    // wafie_process_request_body(&request1);
+    wafie_cleanup(&request1);
+
+
+
+    free(headers1);
+
+    char *audit_content = read_file("/tmp/modsec_audit.log");
+    if (audit_content) {
+        printf("Config loaded: %zu bytes\n", strlen(audit_content));
+        fprintf(stdout, "%s\n", audit_content);
+        free(audit_content);
+    }
+}
